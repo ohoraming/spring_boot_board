f6f73dd0478ff2641aed672272b3f5ef2063c745
ohoraming <kminsmile@gmail.com>
2022-07-05 21:10:26 +0900

    entity, repository, service 생성

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/write") // localhost:8080/board/write
     public String boardwriteForm() {
@@ -14,9 +19,12 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardwritePro(String title, String content) {
-        System.out.println("제목: " + title);
-        System.out.println("내용: " + content);
+    public String boardwritePro(Board board) {
+//        String title, String content로 일일이 전송받지 않고 Board자체를 @Entity를 이용해 받음
+        System.out.println("title" + board.getTitle());
+        System.out.println("content" + board.getContent());
+
+        boardService.write(board); // db에 저장
         return "redirect:/";
     }
 
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,38 @@
+package com.study.board.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Board {
+//    해당 class는 db의 table를 의미
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String title;
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
